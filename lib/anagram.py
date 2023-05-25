8aae97f0dbd6743157ca4ca5540ea1ebb01bb4bc
kwamebrowne <theeecryptoking@gmail.com>
2023-05-24 22:17:06 -0500

    finished basic seach functionality

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word="word"):
+        self.word = word
+    
+    def match(self, list):
+        matches = []
+        for result in list:
+            if sorted(result.casefold()) == sorted(self.word.casefold()) and result.casefold() != self.word.casefold():
+                matches.append(result) 
+        return matches
+    
+# .lower and .casefold same but not with islower
